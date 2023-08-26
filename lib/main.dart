306135c2f5098c4b950901db7144082261a6d4c7
hadez2505 <hadez2505@gmail.com>
2023-08-26 06:27:31 -0500

    bloqueo de orientacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:activos_app/config/router/app_router.dart';
 import 'package:activos_app/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const ProviderScope(child: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
