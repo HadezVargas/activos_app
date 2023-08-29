dcdca4a33db4321441c5632f5463d47524b7cf72
hadez2505 <hadez2505@gmail.com>
2023-08-28 19:49:44 -0500

    excel impl

--- lib/infrastructure/services/excel_generator_service_impl.dart
+++ lib/infrastructure/services/excel_generator_service_impl.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:io';
 
 import 'package:open_filex/open_filex.dart';
@@ -10,37 +8,39 @@ import '../../domain/domain.dart';
 
 class ExcelGeneratorServiceImpl extends ExcelGeneratorService {
   @override
-  Future<void> generateExcelFile(List<Activo> activos) async{
+  Future<void> generateExcelFile(List<Activo> activos) async {
     final Workbook workbook = Workbook();
     final Worksheet sheet = workbook.worksheets[0];
 
     final hedersLabels = [
-        'numberActiveMaximo',
-        'numberJDE',
-        'description',
-        'tAG',
-        'location',
-        'descriptionLocation',
-        'subRegionCommuneCorregimiento',
-        'installation',
-        'father',
-        'state',
-        'iPSIGMA',
-        'series',
-        'criticisms',
-        'criticalityDescription',
-        'operationalNumber',
-        'classification',
-        'addressInternalLocation',
-        'plant',
-        'images',
+      'numberActiveMaximo',
+      'numberJDE',
+      'description',
+      'tAG',
+      'location',
+      'descriptionLocation',
+      'subRegionCommuneCorregimiento',
+      'installation',
+      'father',
+      'state',
+      'iPSIGMA',
+      'series',
+      'criticisms',
+      'criticalityDescription',
+      'operationalNumber',
+      'classification',
+      'addressInternalLocation',
+      'plant',
+      'images',
     ];
 
     for (var i = 0; i < hedersLabels.length; i++) {
-      sheet.getRangeByIndex(1, i+1).setText(hedersLabels[i]);
+      sheet.getRangeByIndex(1, i + 1).setText(hedersLabels[i]);
     }
 
     int rowIndex = 2;
+    int columnIndex = 19;
+    int pictureIndex = 0;
     for (var activo in activos) {
       sheet.getRangeByIndex(rowIndex, 1).setText(activo.numberActiveMaximo);
       sheet.getRangeByIndex(rowIndex, 2).setText(activo.numberJDE);
@@ -48,22 +48,41 @@ class ExcelGeneratorServiceImpl extends ExcelGeneratorService {
       sheet.getRangeByIndex(rowIndex, 4).setText(activo.tAG);
       sheet.getRangeByIndex(rowIndex, 5).setText(activo.location);
       sheet.getRangeByIndex(rowIndex, 6).setText(activo.descriptionLocation);
-      sheet.getRangeByIndex(rowIndex, 7).setText(activo.subRegionCommuneCorregimiento);
+      sheet
+          .getRangeByIndex(rowIndex, 7)
+          .setText(activo.subRegionCommuneCorregimiento);
       sheet.getRangeByIndex(rowIndex, 8).setText(activo.installation);
       sheet.getRangeByIndex(rowIndex, 9).setText(activo.father);
       sheet.getRangeByIndex(rowIndex, 10).setText(activo.state);
       sheet.getRangeByIndex(rowIndex, 11).setText(activo.iPSIGMA);
       sheet.getRangeByIndex(rowIndex, 12).setText(activo.series);
       sheet.getRangeByIndex(rowIndex, 13).setText(activo.criticisms.toString());
-      sheet.getRangeByIndex(rowIndex, 14).setText(activo.criticalityDescription);
+      sheet
+          .getRangeByIndex(rowIndex, 14)
+          .setText(activo.criticalityDescription);
       sheet.getRangeByIndex(rowIndex, 15).setText(activo.operationalNumber);
       sheet.getRangeByIndex(rowIndex, 16).setText(activo.classification);
-      sheet.getRangeByIndex(rowIndex, 17).setText(activo.addressInternalLocation);
+      sheet
+          .getRangeByIndex(rowIndex, 17)
+          .setText(activo.addressInternalLocation);
       sheet.getRangeByIndex(rowIndex, 18).setText(activo.plant);
-      sheet.getRangeByIndex(rowIndex, 19).setText(activo.images.join(', '));
+      // sheet.getRangeByIndex(rowIndex, 19).setText(activo.images.join(', '));
+      for (var image in activo.images) {
+        final List<int> bytes = await File(image).readAsBytes();
+        sheet.pictures.addStream(rowIndex, columnIndex, bytes);
+        final Picture picture = sheet.pictures[pictureIndex];
+        picture.height = 100;
+        picture.width = 100;
+        picture.row = rowIndex;
+        // picture.lastRow = rowIndex;
+        picture.column = columnIndex;
+        // picture.lastColumn = columnIndex;
+        columnIndex++;
+        pictureIndex++;
+      }
       rowIndex++;
+      columnIndex = 19;
     }
-
 
     final List<int> bytes = workbook.saveAsStream();
     workbook.dispose();
@@ -74,6 +93,4 @@ class ExcelGeneratorServiceImpl extends ExcelGeneratorService {
     await file.writeAsBytes(bytes, flush: true);
     OpenFilex.open(path);
   }
-
-  
 }
