ef0da65f98532552fed10f057515c26eeeadab10
hadez2505 <hadez2505@gmail.com>
2023-09-29 18:06:30 -0500

    estilo excel

--- lib/infrastructure/services/excel_generator_service_syncfusion_impl.dart
+++ lib/infrastructure/services/excel_generator_service_syncfusion_impl.dart
@@ -42,14 +42,15 @@ class ExcelGeneratorServiceSyncfusionImpl extends ExcelGeneratorService {
     for (var i = 0; i < hedersLabels.length; i++) {
       sheet.getRangeByIndex(1, i + 1).setText(hedersLabels[i]);
       final Range titulos = sheet.getRangeByIndex(1, i + 1);
+      titulos.autoFit();
       final Style style = titulos.cellStyle;
       style.bold = true;
     }
 
     int rowIndex = 2;
-    int columnIndex = 22;
+    int columnIndex = 21;
     int pictureIndex = 0;
-    for (var activo in activos) {
+    for (final activo in activos) {
       sheet.getRangeByIndex(rowIndex, 1).setText(activo.numberJDE);
       sheet.getRangeByIndex(rowIndex, 2).setText(activo.numberActiveMaximo);
       sheet.getRangeByIndex(rowIndex, 3).setText(activo.tAG);
@@ -58,45 +59,40 @@ class ExcelGeneratorServiceSyncfusionImpl extends ExcelGeneratorService {
       sheet.getRangeByIndex(rowIndex, 6).setText(activo.description2);
       sheet.getRangeByIndex(rowIndex, 7).setText(activo.description3);
       sheet.getRangeByIndex(rowIndex, 8).setText(activo.plant);
-      sheet
-          .getRangeByIndex(rowIndex, 9)
-          .setText(activo.criticalityDescription);
+      sheet.getRangeByIndex(rowIndex, 9).setText(activo.criticalityDescription);
       sheet.getRangeByIndex(rowIndex, 10).setText(activo.location);
       sheet.getRangeByIndex(rowIndex, 11).setText(activo.descriptionLocation);
-      sheet.getRangeByIndex(rowIndex, 12).setText(activo.subRegionCommuneCorregimiento);
+      sheet
+          .getRangeByIndex(rowIndex, 12)
+          .setText(activo.subRegionCommuneCorregimiento);
       sheet.getRangeByIndex(rowIndex, 13).setText(activo.installation);
       sheet.getRangeByIndex(rowIndex, 14).setText(activo.series);
       sheet.getRangeByIndex(rowIndex, 15).setText(activo.father);
-      sheet
-          .getRangeByIndex(rowIndex, 16)
-          .setText(activo.state);
+      sheet.getRangeByIndex(rowIndex, 16).setText(activo.state);
       sheet.getRangeByIndex(rowIndex, 17).setText(activo.iPSIGMA);
       sheet.getRangeByIndex(rowIndex, 18).setText(activo.operationalNumber);
+      sheet.getRangeByIndex(rowIndex, 19).setText(activo.classification);
       sheet
-          .getRangeByIndex(rowIndex, 19)
-          .setText(activo.classification);
-      sheet.getRangeByIndex(rowIndex, 20).setText(activo.addressInternalLocation);
-      sheet.getRangeByIndex(rowIndex, 21).setText(activo.operationalNumber);
-      // sheet.getRangeByIndex(rowIndex, 19).setText(activo.images.join(', '));
-      for (var image in activo.images) {
+          .getRangeByIndex(rowIndex, 20)
+          .setText(activo.addressInternalLocation);
+      for (final image in activo.images) {
         final List<int> bytes = await File(image).readAsBytes();
         sheet.pictures.addStream(rowIndex, columnIndex, bytes);
         final Picture picture = sheet.pictures[pictureIndex];
         picture.height = 100;
         picture.width = 100;
         picture.row = rowIndex;
-        // picture.lastRow = rowIndex;
         picture.column = columnIndex;
-        // picture.lastColumn = columnIndex;
+        sheet.getRangeByIndex(rowIndex, columnIndex).columnWidth = 14.0;
+        // sheet.getRangeByIndex(rowIndex, columnIndex).rowHeight = 88.0;
         columnIndex++;
         pictureIndex++;
       }
-      rowIndex++;
       final Range range = sheet.getRangeByIndex(rowIndex, columnIndex);
       range.autoFit();
-      // range.columnWidth = 14;
-      // range.rowHeight = 88;
-      columnIndex = 22;
+      range.rowHeight = 88;
+      rowIndex++;
+      columnIndex = 21;
     }
 
     final List<int> bytes = workbook.saveAsStream();
